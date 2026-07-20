ded81bb4e7ab15f9c09b8bdd2e04d3a4974f3836
Tushar Mayekar <tusharmayekar95@gmail.com>
2026-07-20 09:07:07 +0530

    Connected dashboard to Google Sheets

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import gspread
+from google.oauth2.service_account import Credentials
+from gspread_dataframe import get_as_dataframe
 
 # =====================================================
 # PAGE CONFIGURATION
